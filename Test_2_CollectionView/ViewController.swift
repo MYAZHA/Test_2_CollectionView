28af7a562a40db846b24026c082ad7af531fc9d1
MYAZHA <y_shelest@mail.ru>
2023-01-15 14:21:32 +0300

    Initial Commit

---
+++ Test_2_CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_2_CollectionView
+//
+//  Created by Юрий Шелест on 15.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
